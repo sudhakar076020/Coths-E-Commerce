441e1e51ba3b268c3a397396c005bd83d2f3e9e7
sudhakar076020 <sudhakar7602m@gmail.com>
2025-04-15 21:44:43 +0530

    nav bar JS added

--- home.js
+++ home.js
@@ -1,1 +1,13 @@
 
+// Nav Bar JS
+const mobileNav = document.querySelector(".hamburger");
+const navbar = document.querySelector(".menubar");
+
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  mobileNav.classList.toggle("hamburger-active");
+};
+mobileNav.addEventListener("click", () => toggleNav());
+
+
+
